2f435eeddc2e0fce0b4bc3676437856452d32d72
AlekzzR <alekzzr@gmail.com>
2021-09-10 21:46:44 +0300

    Добавление основы и списка ссылок

---
+++ Параметризация тестов.py
@@ -0,0 +1,36 @@
+import pytest
+import time
+import math
+from selenium import webdriver
+
+link1 = 'https://stepik.org/lesson/236895/step/1'
+answer = math.log(int(time.time()))
+
+list = [
+'https://stepik.org/lesson/236895/step/1',
+'https://stepik.org/lesson/236896/step/1',
+'https://stepik.org/lesson/236897/step/1',
+'https://stepik.org/lesson/236898/step/1',
+'https://stepik.org/lesson/236899/step/1',
+'https://stepik.org/lesson/236903/step/1',
+'https://stepik.org/lesson/236904/step/1',
+'https://stepik.org/lesson/236905/step/1',
+]
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    browser.implicitly_wait(10)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+@pytest.mark.parametrize('language', ["ru", "en-gb"])
+def test_guest_should_see_login_link(browser, language):
+    link = f"http://selenium1py.pythonanywhere.com/{language}/"
+    browser.get(link)
+    browser.find_element_by_css_selector("#login_link")
+class TestMain():
+    def test_enter_answer(self, browser):
+
