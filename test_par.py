3b85a59dbbb3e3053abe369c9205c54958feae0c
AlekzzR <alekzzr@gmail.com>
2021-09-10 22:24:42 +0300

    Изменена концепция с использованием класса

--- test_par.py
+++ test_par.py
@@ -19,27 +19,30 @@ answer = math.log(int(time.time()))
 'https://stepik.org/lesson/236904/step/1',
 'https://stepik.org/lesson/236905/step/1',
 ]'''
-list = ['https://stepik.org/lesson/236895/step/1']
+
 
 @pytest.fixture(scope="function")
 def browser():
     print("\nstart browser for test..")
     browser = webdriver.Chrome()
-    browser.implicitly_wait(10)
     yield browser
     print("\nquit browser..")
     browser.quit()
-@pytest.mark.parametrize('url', list)
-def test_guest_should_see_login_link(browser, url):
-    link = url
-    browser.get(link)
-    area = WebDriverWait(browser, 5).until(EC.visibility_of_element_located((By.TAG_NAME, 'textarea')))
-    area.send_keys(str(math.log(int(time.time()))))
-    button = WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".submit-submission")))
-    button.click()
-    answer = browser.find_element_by_tag_name('pre')
-    text_answer = answer.text
-    print(text_answer)
-    assert text_answer == 'Correct!', 'Answer wrong'
+class Test_par():
+    ans = ''
+    list = ['https://stepik.org/lesson/236895/step/1']
+    @pytest.mark.parametrize('url', list)
+    def test_par(self, browser, url):
+        link = url
+        browser.implicitly_wait(10)
+        browser.get(link)
+        area = browser.find_element_by_tag_name('textarea')
+        area.send_keys(str(math.log(int(time.time()))))
+        button = WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".submit-submission")))
+        button.click()
+        answer = WebDriverWait(browser, 5).until(EC.visibility_of_element_located((By.TAG_NAME, 'pre')))
+        text_answer = answer.text
+        print(text_answer)
+        assert text_answer == 'Correct!', 'Answer wrong'
 
 
