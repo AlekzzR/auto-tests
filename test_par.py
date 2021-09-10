61963b7c0876a9a218d2318ac36e1a787a07a8fa
AlekzzR <alekzzr@gmail.com>
2021-09-10 22:11:37 +0300

    Изменены параметры доступа к кнопке и области заполнения с использованием "явных ожиданий"

--- test_par.py
+++ test_par.py
@@ -2,11 +2,14 @@ import pytest
 import time
 import math
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 link1 = 'https://stepik.org/lesson/236895/step/1'
 answer = math.log(int(time.time()))
 
-list = [
+'''list = [
 'https://stepik.org/lesson/236895/step/1',
 'https://stepik.org/lesson/236896/step/1',
 'https://stepik.org/lesson/236897/step/1',
@@ -15,7 +18,8 @@ list = [
 'https://stepik.org/lesson/236903/step/1',
 'https://stepik.org/lesson/236904/step/1',
 'https://stepik.org/lesson/236905/step/1',
-]
+]'''
+list = ['https://stepik.org/lesson/236895/step/1']
 
 @pytest.fixture(scope="function")
 def browser():
@@ -29,9 +33,9 @@ def browser():
 def test_guest_should_see_login_link(browser, url):
     link = url
     browser.get(link)
-    area = browser.find_element_by_tag_name('textarea')
+    area = WebDriverWait(browser, 5).until(EC.visibility_of_element_located((By.TAG_NAME, 'textarea')))
     area.send_keys(str(math.log(int(time.time()))))
-    button = browser.find_element_by_css_selector(".submit-submission")
+    button = WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".submit-submission")))
     button.click()
     answer = browser.find_element_by_tag_name('pre')
     text_answer = answer.text
