99b0e660da8dbd05fb00a6fa4c4f38a69e99625c
AlekzzR <alekzzr@gmail.com>
2021-09-10 21:51:39 +0300

    Первый возможный рабочий вариант

--- Параметризация тестов.py
+++ Параметризация тестов.py
@@ -25,12 +25,17 @@ def browser():
     yield browser
     print("\nquit browser..")
     browser.quit()
-
-@pytest.mark.parametrize('language', ["ru", "en-gb"])
-def test_guest_should_see_login_link(browser, language):
-    link = f"http://selenium1py.pythonanywhere.com/{language}/"
+@pytest.mark.parametrize('url', list)
+def test_guest_should_see_login_link(browser, url):
+    link = url
     browser.get(link)
-    browser.find_element_by_css_selector("#login_link")
-class TestMain():
-    def test_enter_answer(self, browser):
+    area = browser.find_element_by_tag_name('textarea')
+    area.send_keys(str(math.log(int(time.time()))))
+    button = browser.find_element_by_css_selector(".submit-submission")
+    button.click()
+    answer = browser.find_element_by_tag_name('pre')
+    text_answer = answer.text
+    print(text_answer)
+    assert text_answer == 'Correct!', 'Answer wrong'
+
 
